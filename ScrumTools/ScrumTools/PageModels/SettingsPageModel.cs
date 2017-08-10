5ba13332c0ea01bbae99453c92837b7cbb9163c5
Ingolmo <ingolmo1@hotmail.com>
2017-08-10 14:49:21 +0200

    Commit from the office

---
+++ ScrumTools/ScrumTools/PageModels/SettingsPageModel.cs
@@ -0,0 +1,33 @@
+﻿using FreshMvvm;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace ScrumTools.PageModels
+{
+    public class SettingsPageModel : FreshBasePageModel
+    {
+        public SettingsPageModel()
+        {
+            InitializeCommands();
+        }
+
+        /* Commands */
+        private ICommand _goToAboutCommand;
+        public ICommand GoToAboutCommand
+        {
+            get { return _goToAboutCommand; }
+        }
+
+        /* Private Methods */
+        private void InitializeCommands()
+        {
+            _goToAboutCommand = new Command(async () => await GoToAboutExecute());
+        }
+
+        private async Task GoToAboutExecute()
+        {
+            await CoreMethods.PushPageModel<AboutPageModel>(null, true, true);
+        }
+    }
+}
