6f624e2333542a8696a2fd0b4189bda507d7b11a
Ingolmo <ingolmo1@hotmail.com>
2017-08-10 11:13:03 +0200

    Add project files.

---
+++ ScrumTools/ScrumTools/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿
+using FreshMvvm;
+using ScrumTools.PageModels;
+using Xamarin.Forms;
+
+namespace ScrumTools
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            SetMainPage();
+
+
+        }
+
+        /* Private Methods */
+
+        private void SetMainPage()
+        {
+            var detail = FreshPageModelResolver.ResolvePageModel<MainPageModel>();
+
+            MainPage = new FreshNavigationContainer(detail);
+        }
+    }
+}
