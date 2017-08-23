6bf2a3a394c231be5337a9e30d7f595187a32c90
Juan López García <ingolmo1@hotmail.com>
2017-08-23 22:04:42 +0200

    Minor display changes at MainPage

--- ScrumTools/ScrumTools/Models/Card.cs
+++ ScrumTools/ScrumTools/Models/Card.cs
@@ -5,6 +5,7 @@
         public string Title { get; set; }
         public string Detail { get; set; }
         public string Image { get; set; }
+        public string Cover { get; set; }
         public string Thumbnail { get; set; }
     }
 }
