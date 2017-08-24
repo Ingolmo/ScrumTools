55433f7554f37bf79baf41de67610e60b18626f7
Ingolmo <ingolmo1@hotmail.com>
2017-08-24 14:51:46 +0200

    UX improvements & Local Storage for settings

--- ScrumTools/ScrumTools/Helpers/Settings.cs
+++ ScrumTools/ScrumTools/Helpers/Settings.cs
@@ -1,43 +1,28 @@
-// Helpers/Settings.cs
 using Plugin.Settings;
 using Plugin.Settings.Abstractions;
 
 namespace ScrumTools.Helpers
 {
-	/// <summary>
-	/// This is the Settings static class that can be used in your Core solution or in any
-	/// of your client applications. All settings are laid out the same exact way with getters
-	/// and setters. 
-	/// </summary>
 	public static class Settings
 	{
-		private static ISettings AppSettings
+        private const string DisplayKey = "display_key";
+
+        private static ISettings AppSettings
 		{
-			get
-			{
-				return CrossSettings.Current;
-			}
+			get	{ return CrossSettings.Current;	}
 		}
 
-		#region Setting Constants
+        public static class CardDisplay
+        {
+            public const string Grid = "grid";
+            public const string List = "list";
 
-		private const string SettingsKey = "settings_key";
-		private static readonly string SettingsDefault = string.Empty;
-
-		#endregion
-
-
-		public static string GeneralSettings
-		{
-			get
-			{
-				return AppSettings.GetValueOrDefault(SettingsKey, SettingsDefault);
-			}
-			set
-			{
-				AppSettings.AddOrUpdateValue(SettingsKey, value);
-			}
-		}
+            public static string Display
+            {
+                get { return AppSettings.GetValueOrDefault(DisplayKey, Grid); }
+                set { AppSettings.AddOrUpdateValue(DisplayKey, value); }
+            }
+        }
 
 	}
 }
